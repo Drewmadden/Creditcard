534b9a061e2ffb2082bb08c427ffaa08fe088e1c
Drew <Drew@Drew-VAIO>
2014-09-30 03:26:05 -0400

    Credit

---
+++ MidTerm2/src/CreditCheckTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CreditCheckTest {
+
+	@Test
+	public void test() {
+		CreditCheck credit = new CreditCheck()
+		String actual = CreditCheck.something(4388576018402626);
+		String result = "4388576018402626";
+	}
+}
